3611ec07f2a5ca3695916d670775dce43651562c
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-07-16 18:10:57 +0300

    Revert back to using ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,6 @@ package com.example.android.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,7 +16,6 @@ public class NumbersActivity extends AppCompatActivity {
 
         // Create a list of words
         ArrayList<String> words = new ArrayList<String>();
-
         words.add("one");
         words.add("two");
         words.add("three");
@@ -28,10 +26,6 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
-
-        for (int i = 11; i < 1000; i++) {
-            words.add(String.valueOf(i));
-        }
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
         // adapter knows how to create layouts for each item in the list, using the
@@ -44,12 +38,12 @@ public class NumbersActivity extends AppCompatActivity {
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // activity_numbers.xml layout file.
-        GridView gridView = (GridView) findViewById(R.id.gridview);
+        ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
         // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        gridView.setAdapter(itemsAdapter);
+        listView.setAdapter(itemsAdapter);
     }
 }
