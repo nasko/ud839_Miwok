469b953f9ca33f9101cb6690b82c11cbc8b49ec8
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-07-16 14:40:10 +0300

    Define onClick listeners inline

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,25 +14,45 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersList(View view) {
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
+        View numbers = findViewById(R.id.numbers);
+        assert numbers != null;
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(i);
+            }
+        });
 
-    public void openFamilyList(View view) {
-        Intent i = new Intent(this, FamilyActivity.class);
-        startActivity(i);
-    }
+        View family = findViewById(R.id.family);
+        assert family != null;
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(i);
+            }
+        });
 
-    public void openColorsList(View view) {
-        Intent i = new Intent(this, ColorsActivity.class);
-        startActivity(i);
-    }
+        View colors = findViewById(R.id.colors);
+        assert colors != null;
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(i);
+            }
+        });
 
-    public void openPhrasesList(View view) {
-        Intent i = new Intent(this, PhrasesActivity.class);
-        startActivity(i);
+        View phrases = findViewById(R.id.phrases);
+        assert phrases != null;
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
