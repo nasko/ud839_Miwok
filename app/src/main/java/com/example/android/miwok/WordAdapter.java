6f4932c8acb1d29c50c806f7af8c1c7c82431f02
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-07-19 20:40:23 +0300

    Implement the ViewHolder pattern for caching of recycled views

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -15,6 +15,13 @@ import java.util.List;
 import static android.R.attr.resource;
 
 public class WordAdapter extends ArrayAdapter<Word> {
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView miwokView;
+        TextView defaultView;
+    }
+
     public WordAdapter(@NonNull Context context, @NonNull List<Word> objects) {
         super(context, 0, objects);
     }
@@ -22,20 +29,33 @@ public class WordAdapter extends ArrayAdapter<Word> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
         // Get the data item for this position
         Word word = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
-        }
-        // Lookup view for data population
-        TextView tvMiwok = (TextView) convertView.findViewById(R.id.miwok_text_view);
-        TextView tvDefault = (TextView) convertView.findViewById(R.id.default_text_view);
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
 
-        // Populate the data into the template view using the data object
+
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+            viewHolder.miwokView = (TextView) convertView.findViewById(R.id.miwok_text_view);
+            viewHolder.defaultView = (TextView) convertView.findViewById(R.id.default_text_view);
+
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
         assert word != null;
-        tvMiwok.setText(word.getMiwokTranslation());
-        tvDefault.setText(word.getDefaultTranslation());
+        viewHolder.miwokView.setText(word.getMiwokTranslation());
+        viewHolder.defaultView.setText(word.getDefaultTranslation());
 
         // Return the completed view to render on screen
         return convertView;
